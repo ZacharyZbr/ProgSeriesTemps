d3feb8a5b399b498a655b12f57637ec5a1d4359e
Zachary Zuber <zuber.zac@gmail.com>
2023-01-31 08:31:42 +0100

    MAJ .gitignore

--- ProgSeriesTemps/ProgSeriesTemps.cpp
+++ ProgSeriesTemps/ProgSeriesTemps.cpp
@@ -8,19 +8,17 @@
 
 int main()
 {
-    std::cout << "Hello World !" << std::endl;
+    
     int theSize = 2;
     gsl_vector* vect = gsl_vector_calloc(theSize);
     gsl_vector_set(vect, 0, 0);
     gsl_vector_set(vect, 1, 1);
     
-    
-
     gsl_rng* r = gsl_rng_alloc(gsl_rng_taus);
     
     //Test simulation gaussienne
+    std::cout << "Simulation Gausienne" << std::endl;
     std::string myName = "Gaussien";
-
     cGaussian* myGauss = new cGaussian(myName, r);
     double simulation;
     for (int j = 0; j < 25; j++) {
@@ -28,16 +26,18 @@ int main()
     }
 
     // Test simulation student
+    std::cout << "Simulation de Student" << std::endl;
     std::string myStudentName = "Student";
     gsl_vector* myParams = gsl_vector_calloc(1);
-    gsl_vector_set(myParams, 0, 2);
+    gsl_vector_set(myParams, 0, 10);
     cStudent* myStudentSimul = new cStudent(myStudentName, myParams, r);
     for (int j = 0; j < 25; j++) {
         std::cout << myStudentSimul->mSimulate() << endl;
     }
 
-
+    gsl_rng_free(r);
     gsl_vector_free(vect);
+    gsl_vector_free(myParams);
     
 
     
